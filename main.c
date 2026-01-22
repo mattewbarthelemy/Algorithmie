cfea09479ea98be977f90423775fdb0c564d1b9d
mattewbarthelemy <mattewbarthelemy@gamingcampus.in>
2026-01-22 19:41:51 +0100

    test

--- main.c
+++ main.c
@@ -56,8 +56,10 @@ int main(void)
     maps[2] = CreateMap("The S", MAP_03);
     maps[3] = CreateMap("Make a Choice", MAP_04);
     maps[4] = CreateMap("The Maze", MAP_05);
-    maps[5] = CreateMap("06", MAP_06);
-    maps[6] = CreateMap("07", MAP_07);
+    maps[5] = CreateMap("Easy", MAP_06);
+    maps[6] = CreateMap("Hello ?", MAP_07);
+    maps[7] = CreateMap("Why ?", MAP_08);
+    maps[8] = CreateMap("The Chaos", MAP_09);
 
     ChangeMap(mapSelectionMenu, &maps[currentMap]);
 
