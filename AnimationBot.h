8fb32f31b3d6d30e7b1a01224165218d63e50d14
mattewbarthelemy <mattewbarthelemy@gamingcampus.in>
2026-01-23 15:36:32 +0100

    update_final

--- AnimationBot.h
+++ AnimationBot.h
@@ -4,19 +4,17 @@
 #include <SFML/System.h>
 #include <stdbool.h>
 
-#define ANIMATION_FRAMES 5  // 3 images par direction
+#define ANIMATION_FRAMES 5
 
-// Direction de l'animation
 typedef enum {
     ANIM_LEFT,
     ANIM_RIGHT
 } AnimDirection;
 
-// Structure d'animation pour le bot avec directions
 typedef struct {
-    // Frames pour chaque direction
-    sfTexture* framesLeft[ANIMATION_FRAMES];   // Gauche
-    sfTexture* framesRight[ANIMATION_FRAMES];  // Droite
+
+    sfTexture* framesLeft[ANIMATION_FRAMES];
+    sfTexture* framesRight[ANIMATION_FRAMES];
 
     int currentFrame;
     AnimDirection currentDirection;
@@ -25,29 +23,20 @@ typedef struct {
     bool isPlaying;
 } BotAnimation;
 
-// Créer et initialiser l'animation
 BotAnimation* CreateBotAnimation(float speed);
 
-// Charger toutes les frames (2 directions)
 bool LoadAnimationFrames(BotAnimation* anim);
 
-// Mettre à jour l'animation
 void UpdateAnimation(BotAnimation* anim, sfSprite* sprite);
 
-// Démarrer l'animation
 void StartAnimation(BotAnimation* anim);
 
-// Arrêter l'animation
 void StopAnimation(BotAnimation* anim, sfSprite* sprite);
 
-// Changer la direction de l'animation
 void SetAnimationDirection(BotAnimation* anim, AnimDirection direction, sfSprite* sprite);
 
-// Détruire l'animation
 void DestroyBotAnimation(BotAnimation* anim);
 
-// Obtenir la frame actuelle
 int GetCurrentFrame(BotAnimation* anim);
 
-// Changer la vitesse
 void SetAnimationSpeed(BotAnimation* anim, float speed);
